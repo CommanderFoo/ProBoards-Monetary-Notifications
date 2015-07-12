f6a7f4fecdcec8fb132e2e78f0c8630f702d893e
Peter <github@pixeldepth.net>
2015-07-12 13:56:38 +0100

    Parsing messages to expand them

--- monetary_notifications.js
+++ monetary_notifications.js
@@ -15,14 +15,18 @@ $(function(){
 				if(this.plugin && typeof yootil != "undefined" && typeof yootil.notifications != "undefined"){
 					new yootil.notifications(this.key, this.settings.notification_template).show({
 
+						// Parse messages.  We keep the msg stored as small as possible and expand it before showing
+
 						before: function(notification){
-							console.log(notification);
+							if(notification.m.match(/^\[D:([\d\,]+)\|(\d+)\|(.+?)\]$/)){
+								notification.m = "You have recevied a donation of " + monetary.settings.money_symbol + monetary.format(RegExp.$1) + " from <a href='/user/" + (~~ RegExp.$2) + "'>" + RegExp.$3 + "</a>.";
+							} else if(notification.m.match(/^\[T:(\d+)\|(.+?)\]$/)){
+								notification.m = "You have recevied a trade request from <a href='/user/" + (~~ RegExp.$2) + "'>" + RegExp.$3 + "</a>.";
+							} else if(notification.m.match(/^\[TG:(\d+)\|(.+?)\]$/)){
+								notification.m = "You have recevied a gift from <a href='/user/" + (~~ RegExp.$2) + "'>" + RegExp.$3 + "</a>.";
+							}
 
 							return notification;
-						},
-
-						after: function(notification){
-							console.log(notification);
 						}
 
 					});
@@ -30,7 +34,7 @@ $(function(){
 			},
 
 			setup: function(){
-				this.plugin = proboards.plugin.get(this.key);
+				this.plugin = pb.plugin.get(this.key);
 				this.settings = (this.plugin && this.plugin.settings)? this.plugin.settings : false;
 			},
 
